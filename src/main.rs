24589a30976d91004ab740229551fa300d90fff9
jaredgorski <jared.gorski@liferay.cloud>
2019-08-17 19:49:32 -0700

    Uncomment main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ mod solutions;
 fn main() {
     println!("\nSTART\n");
 
-    // solutions::p001::main();
-    // solutions::p002::main();
-    // solutions::p003::main();
-    // solutions::p004::main();
-    // solutions::p005::main();
-    // solutions::p006::main();
-    // solutions::p007::main();
+    solutions::p001::main();
+    solutions::p002::main();
+    solutions::p003::main();
+    solutions::p004::main();
+    solutions::p005::main();
+    solutions::p006::main();
+    solutions::p007::main();
     solutions::p008::main();
 
     println!("\nEND\n");
